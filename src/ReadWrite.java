4d263db759d7eeaf3bff764c9590c20c4bcff6f8
DanCtdsb <daniel.chen21@student.tdsb.on.ca>
2025-03-25 21:46:22 -0400

    Update ReadWrite.java

--- src/ReadWrite.java
+++ src/ReadWrite.java
@@ -1,28 +1,29 @@
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 
 
 public class ReadWrite {
     public static void main(String[] args) throws IOException{
-        FileReader reader = null;
+        BufferedReader reader = null;
         FileWriter writer = null;
         double sum = 0;
         int counter = 0;
+        String num = null;
         try {
-            reader = new FileReader("numbers.txt");
-            double num = reader.read();
-            while (reader.read() != -1) {
+            reader = new BufferedReader(new FileReader("numbers.txt"));
+            while ((num = reader.readLine()) != null) {
+                sum += Integer.parseInt(num);
                 counter++;
-
             }
         } catch (FileNotFoundException exception) {
             System.out.println("File not found");
         } finally {
             reader.close();
         }
-        writer = new FileWriter("average.txt");
+        writer = new FileWriter("average.txt", true);
         writer.write("Your average is: " + sum/counter);
 
         writer.close();
