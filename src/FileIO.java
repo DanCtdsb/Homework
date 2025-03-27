6caba78ebcc06cbf3fb3a07f9f1aedeef2230bb4
unknown <Daniel.Chen21@student.tdsb.on.ca>
2025-03-27 11:32:43 -0400

    FileIO.java

--- src/FileIO.java
+++ src/FileIO.java
@@ -5,31 +5,29 @@ import java.io.IOException;
 import java.io.FileNotFoundException;
 
 
-public class ReadWrite {
+public class FileIO {
     public static void main(String[] args) throws IOException{
         BufferedReader reader = null;
         FileWriter writer = null;
         double sum = 0;
         int counter = 0;
         String num = null;
+
         try {
             reader = new BufferedReader(new FileReader("numbers.txt"));
             while ((num = reader.readLine()) != null) {
                 sum += Double.parseDouble(num);
                 counter++;
+            writer = new FileWriter("average.txt", true);
+            writer.write("Your average is: " + sum/counter);
             }
         } catch (FileNotFoundException exception) {
-            System.out.println("File not found");
+            System.out.println("Error: File not found");
+        } catch (IOException exception) {
+            System.out.println("Error: Cannot create/locate file");
         } finally {
             reader.close();
+            writer.close();
         }
-        writer = new FileWriter("average.txt", true);
-        writer.write("Your average is: " + sum/counter);
-
-        writer.close();
-
-
     }
-
-
 }
