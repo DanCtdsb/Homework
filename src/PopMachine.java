cd4008d2f1ec42139b1d3d068075761939a8af9e
unknown <343025052@TDSB.ON.CA>
2025-04-02 11:40:20 -0400

    d

---
+++ src/PopMachine.java
@@ -0,0 +1,63 @@
+public class PopMachine {        
+    private double amountofMoney;
+    private int numberOfCans;
+    private double pricePerCans;
+
+    public PopMachine(double amountofMoney, int numberOfCans, double pricePerCans) {
+        this.amountofMoney = amountofMoney;
+        this.numberOfCans = numberOfCans;
+        this.pricePerCans = pricePerCans;
+    }
+
+    private boolean disperseCan() {
+        if (numberOfCans > 0) {
+            numberOfCans--;
+            System.out.println("One can dispersed, remaining cans: " + numberOfCans);
+            return true;
+        } else {
+            System.out.println("Action rejected: Not enough cans to disperse");
+            return false;
+        }
+    }
+
+    private void coinSlot(double money) {
+        if (money < pricePerCans) {
+            System.out.println("Action rejected: Not enough money");
+            return;
+        }
+        amountofMoney += money;
+        giveChange(money);
+        boolean dispersed = disperseCan();
+        if (!dispersed) {
+            System.out.println("An Error has occured, your refund is $" + refund(dispersed));
+        }
+    }
+
+    private void giveChange(double money) {
+        if (money > pricePerCans) {
+            System.out.println("You change is $" + (money - pricePerCans));
+        }
+
+    }
+    private double refund(boolean dispersed) {
+        amountofMoney -= pricePerCans; 
+        if (dispersed) {
+            numberOfCans++;
+        }
+        return pricePerCans;
+    }
+
+    public static void main(String[] args) {
+        // Ask the user for the n value
+        double money = 200;
+        PopMachine popMachine = new PopMachine(10.0, 100, 1.5);
+        while (money > 0) {
+            
+            popMachine.coinSlot(1.5);
+            money -= 1.5;
+            System.out.println(money);
+        }
+    }
+}
+
+
